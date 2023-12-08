7c27cee0c9965288cc076c3024cc32c8b6ce679a
whoopty1234 <jcho235@students.fairmontschools.com>
2023-12-08 11:31:47 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 public class Main {
+
+//main method that calls upon the other classes to play the game
     public static void main(String[] args) {
         BlackjackGame blackjackGame = new BlackjackGame();
         blackjackGame.play();
